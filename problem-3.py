6b92216a92b7803cf9bbc81b44e83fadfd6c8a57
Mikdad Mohammad Asif <asifmikdad@gmail.com>
2022-10-22 19:47:31 +0600

    update

---
+++ problem-3.py
@@ -0,0 +1,20 @@
+"""
+Game = Guess a number
+requirement -
+1. user instruction
+2. delay
+formula = (2x+8)/2 -x
+"""
+from time import sleep
+
+print("Think about a number")
+sleep(2)
+print("Multiply 2 with your number")
+sleep(2)
+print("Add 8 with your number")
+sleep(2)
+print("Divide the whole result")
+sleep(2)
+print("Subtract the guessed number from the result")
+sleep(2)
+print("Your guessed number is 4")
